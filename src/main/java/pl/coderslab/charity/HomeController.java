058050cdf12941448533cd1ea34c7cd68578f7b5
Monika Misiewicz <zieba.monika@wp.pl>
2020-03-10 17:14:33 +0100

    added method institutions() to InstitutionService and getList() to HomeController

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -1,16 +1,31 @@
 package pl.coderslab.charity;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.Institution.InstitutionRepository;
+import pl.coderslab.charity.Institution.InstitutionService;
 
 
 @Controller
+@RequiredArgsConstructor
+
 public class HomeController {
+
+    private final InstitutionService institutionService;
 
 
     @RequestMapping("/")
     public String homeAction(Model model){
         return "index";
+    }
+
+
+    @GetMapping("/institutions")
+    public String getList(Model model) {
+        model.addAttribute("institutions", institutionService.institutions());
+        return "institutions/list";
     }
 }
--- src/main/java/pl/coderslab/charity/Institution/InstitutionService.java
+++ src/main/java/pl/coderslab/charity/Institution/InstitutionService.java
@@ -1,6 +1,20 @@
 package pl.coderslab.charity.Institution;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class InstitutionService {
 
+    private final InstitutionRepository institutionRepository;
+
+    public List<Institution> institutions() {
+        return institutionRepository.findAll();
+    }
 
 }
