f2859d267058d545ba0ad80ecc2aa7868e187cc3
Monika Misiewicz <zieba.monika@wp.pl>
2020-03-11 20:25:57 +0100

    created DonationController

---
+++ src/main/java/pl/coderslab/charity/Donation/DonationController.java
@@ -0,0 +1,11 @@
+package pl.coderslab.charity.Donation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class DonationController {
+
+
+}
