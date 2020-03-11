bd3330858ebb8d6ef9688f5d07e4c9289f3edb4b
Monika Misiewicz <zieba.monika@wp.pl>
2020-03-11 10:37:45 +0100

    added data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO institution (id, name, description) VALUES (1, 'Fundacja "Dbam o Zdrowie"', 'Cel i misja: Pomoc dzieciom z ubogich rodzin.');
+INSERT INTO institution (id, name, description) VALUES (2, 'Fundacja "A kogo"', 'Cel i misja: Pomoc wybudzaniu dzieci ze śpiączki.');
+INSERT INTO institution (id, name, description) VALUES (3, 'Fundacja “Dla dzieci"', 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.');
+INSERT INTO institution (id, name, description) VALUES (4, 'Fundacja “Bez domu”', 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania');
