5e98460db646dda434cebc9ab760084cde30bd2c
Monika Misiewicz <zieba.monika@wp.pl>
2020-03-12 17:11:40 +0100

    added new two methods to DonationController

--- src/main/java/pl/coderslab/charity/Donation/DonationController.java
+++ src/main/java/pl/coderslab/charity/Donation/DonationController.java
@@ -2,10 +2,49 @@ package pl.coderslab.charity.Donation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.coderslab.charity.Category.Category;
+import pl.coderslab.charity.Category.CategoryRepository;
+import pl.coderslab.charity.Institution.Institution;
+import pl.coderslab.charity.Institution.InstitutionRepository;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class DonationController {
 
+    private final DonationRepository donationRepository;
+    private final InstitutionRepository institutionRepository;
+    private final CategoryRepository categoryRepository;
 
+    @GetMapping("/add-donation")
+    public String addDonation(Model model) {
+        model.addAttribute("donation", new Donation());
+        return "form";
+    }
+
+    @PostMapping("/save-donation")
+    public String saveDonation(@ModelAttribute("donation") @Valid Donation donation, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "form";
+        }
+        donationRepository.save(donation);
+        return "redirect:/form-confirmation";
+    }
+
+    @ModelAttribute("institutions")
+    public List<Institution> institutions() {
+        return institutionRepository.findAll();
+    }
+
+    @ModelAttribute("categories")
+    public List<Category> categories() {
+        return categoryRepository.findAll();
+    }
 }
