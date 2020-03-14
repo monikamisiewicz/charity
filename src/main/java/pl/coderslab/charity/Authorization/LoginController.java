9d137f45f2a5f583c29b786486a5012a97441adb
Monika Misiewicz <zieba.monika@wp.pl>
2020-03-14 17:30:47 +0100

    correcte loginController

--- src/main/java/pl/coderslab/charity/Authorization/LoginController.java
+++ src/main/java/pl/coderslab/charity/Authorization/LoginController.java
@@ -4,9 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -15,27 +15,38 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class LoginController {
 
-    private UserService userService;
+    private final UserService userService;
 
-    @RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
-    public ModelAndView login(){
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("login");
-        return modelAndView;
+//    @RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+//    public ModelAndView login(){
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.setViewName("login");
+//        return modelAndView;
+//    }
+
+    @GetMapping(value = {"/", "/login"})
+    public String login() {
+        return "login";
     }
 
-    @RequestMapping(value="/registration", method = RequestMethod.GET)
-    public ModelAndView registration(){
-        ModelAndView modelAndView = new ModelAndView();
+//    @RequestMapping(value="/registration", method = RequestMethod.GET)
+//    public ModelAndView registration(){
+//        ModelAndView modelAndView = new ModelAndView();
+//        User user = new User();
+//        modelAndView.addObject("user", user);
+//        modelAndView.setViewName("registration");
+//        return modelAndView;
+//    }
+
+    @GetMapping("/registration")
+    public String register(Model model) {
         User user = new User();
-        modelAndView.addObject("user", user);
-        modelAndView.setViewName("registration");
-        return modelAndView;
+        model.addAttribute("user", user);
+        return "registration";
     }
 
-    @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
-        ModelAndView modelAndView = new ModelAndView();
+    @PostMapping("/registration")
+    public String createNewUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult, Model model) {
         User userExists = userService.findUserByUserName(user.getUserName());
         if (userExists != null) {
             bindingResult
@@ -43,24 +54,53 @@ public class LoginController {
                             "Użytkownik z podaną nazwą już istnieje");
         }
         if (bindingResult.hasErrors()) {
-            modelAndView.setViewName("registration");
+            return ("registration");
         } else {
             userService.saveUser(user);
-            modelAndView.addObject("successMessage", "Użytkownik został pomyślnie zarejestrowany");
-            modelAndView.addObject("user", new User());
-            modelAndView.setViewName("registration");
+            model.addAttribute("successMessage", "Użytkownik został pomyślnie zarejestrowany");
+            model.addAttribute("user", new User());
         }
-        return modelAndView;
+        return "registration";
     }
 
-    @RequestMapping(value="/admin/index", method = RequestMethod.GET)
-    public ModelAndView home(){
-        ModelAndView modelAndView = new ModelAndView();
+//    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+//    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+//        ModelAndView modelAndView = new ModelAndView();
+//        User userExists = userService.findUserByUserName(user.getUserName());
+//        if (userExists != null) {
+//            bindingResult
+//                    .rejectValue("userName", "error.user",
+//                            "Użytkownik z podaną nazwą już istnieje");
+//        }
+//        if (bindingResult.hasErrors()) {
+//            modelAndView.setViewName("registration");
+//        } else {
+//            userService.saveUser(user);
+//            modelAndView.addObject("successMessage", "Użytkownik został pomyślnie zarejestrowany");
+//            modelAndView.addObject("user", new User());
+//            modelAndView.setViewName("registration");
+//        }
+//        return modelAndView;
+//    }
+
+    @GetMapping("/admin/index")
+    public String home(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = userService.findUserByUserName(auth.getName());
-        modelAndView.addObject("userName", "Witaj " + user.getFirstName());
-        modelAndView.addObject("adminMessage","Ta zawartość dostępna jest tylko dla Administratora");
-        modelAndView.setViewName("admin/index");
-        return modelAndView;
+        model.addAttribute("userName", "Witaj " + user.getFirstName());
+        model.addAttribute("adminMessage", "Ta zawartość dostępna jest tylko dla Administratora");
+        return"admin/index";
+
     }
+
+//    @RequestMapping(value = "/admin/index", method = RequestMethod.GET)
+//    public ModelAndView home() {
+//        ModelAndView modelAndView = new ModelAndView();
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        User user = userService.findUserByUserName(auth.getName());
+//        modelAndView.addObject("userName", "Witaj " + user.getFirstName());
+//        modelAndView.addObject("adminMessage", "Ta zawartość dostępna jest tylko dla Administratora");
+//        modelAndView.setViewName("admin/index");
+//        return modelAndView;
+//    }
 }
